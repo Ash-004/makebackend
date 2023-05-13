f06c53f44a4d610ec5adcee89a7294012890d6fb
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-05-13 17:29:32 +0530

    made an api wrapper around the cli wrapper

--- test.js
+++ test.js
@@ -1,30 +1,32 @@
 const CharacterAI = require('node_characterai');
-const readline = require('readline');
+const express = require('express');
+const bodyParser = require('body-parser');
 const characterAI = new CharacterAI();
 
-(async() => {
-    await characterAI.authenticateWithToken("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkVqYmxXUlVCWERJX0dDOTJCa2N1YyJ9.eyJpc3MiOiJodHRwczovL2NoYXJhY3Rlci1haS51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjQ0NTIwN2ZjN2YwZWM1NTE3OTE1YWYxIiwiYXVkIjpbImh0dHBzOi8vYXV0aDAuY2hhcmFjdGVyLmFpLyIsImh0dHBzOi8vY2hhcmFjdGVyLWFpLnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2ODIyNTE5NDMsImV4cCI6MTY4NDg0Mzk0MywiYXpwIjoiZHlEM2dFMjgxTXFnSVNHN0Z1SVhZaEwyV0VrbnFaenYiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.Va5ovKA1mvUJGdMmO9Mzg_RiY06z2PD101WF0EeJfILGc1lQk0c0NZU5-DyCvykDyKO-CJq5VQuWWDsM4wl3OHhJRVG5iO7CsJ1KzW1LQA8Vvr99wwWbYpZbdnO2pnNhxacihVSQUR2xMLA8ZeWyEfTHs0YQpMpsfQ4zfoubM0vUECvQNYQYzHbX8s_rbHPm3E_dKU9kQcc147wzL-UFMBYkPzAgL7xHCYnBBc4s50lKfSumU4GU0vh_6p4Zq4zjl9asXZsrh65WqyuYt1gMOKiWk3VHEVqFvTomOQzLBTdklb6eL6OPYf3eqwVjeUAJD-um2IlyNjvn5_lcCruNeQ");
+const app = express();
+const port = 3000;
 
-    const characterId = "iV5qb8ttzD7Ytl69U_-ONcW2tW_lrFrOVKExyKJHlJM"; // Discord moderator
+(async () => {
+  await characterAI.authenticateWithToken("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkVqYmxXUlVCWERJX0dDOTJCa2N1YyJ9.eyJpc3MiOiJodHRwczovL2NoYXJhY3Rlci1haS51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjQ0NTIwN2ZjN2YwZWM1NTE3OTE1YWYxIiwiYXVkIjpbImh0dHBzOi8vYXV0aDAuY2hhcmFjdGVyLmFpLyIsImh0dHBzOi8vY2hhcmFjdGVyLWFpLnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2ODIyNTE5NDMsImV4cCI6MTY4NDg0Mzk0MywiYXpwIjoiZHlEM2dFMjgxTXFnSVNHN0Z1SVhZaEwyV0VrbnFaenYiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.Va5ovKA1mvUJGdMmO9Mzg_RiY06z2PD101WF0EeJfILGc1lQk0c0NZU5-DyCvykDyKO-CJq5VQuWWDsM4wl3OHhJRVG5iO7CsJ1KzW1LQA8Vvr99wwWbYpZbdnO2pnNhxacihVSQUR2xMLA8ZeWyEfTHs0YQpMpsfQ4zfoubM0vUECvQNYQYzHbX8s_rbHPm3E_dKU9kQcc147wzL-UFMBYkPzAgL7xHCYnBBc4s50lKfSumU4GU0vh_6p4Zq4zjl9asXZsrh65WqyuYt1gMOKiWk3VHEVqFvTomOQzLBTdklb6eL6OPYf3eqwVjeUAJD-um2IlyNjvn5_lcCruNeQ");
 
-    const chat = await characterAI.createOrContinueChat(characterId);
+  const characterId = "Hpk0GozjACb3mtHeAaAMb0r9pcJGbzF317I_Ux_ALOA"; // psychologist
+  const chat = await characterAI.createOrContinueChat(characterId);
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+  app.use(bodyParser.json());
+  
+  app.post('/chat', async (req, res) => {
+    const input = req.body.text;
+    console.log('user-'+input)
+    const response = await chat.sendAndAwaitResponse(input, true);
+    console.log('bot-'+response.text)
+    res.send({"text": response.text});
+  });
 
-    rl.setPrompt('You: ');
-    rl.prompt();
+  app.get('/active', async (req, res) => {
+    res.status(200).send({ "active": "true" });
+  });
 
-    rl.on('line', async (input) => {
-        const response = await chat.sendAndAwaitResponse(input, true);
-        console.log('Bot:', response.text);
-        rl.prompt();
-    });
-
-    rl.on('close', () => {
-        console.log('Exiting...');
-        process.exit(0);
-    });
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
 })();
